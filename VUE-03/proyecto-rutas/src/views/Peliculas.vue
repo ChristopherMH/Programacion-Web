b37459b62dc8c9e274035bb2aff3ce6029ceee92
Christopher <christophermelendez13@gmail.com>
2021-04-27 16:48:56 -0600

    Haciendo peticiones sobre una API

--- VUE-03/proyecto-rutas/src/views/Peliculas.vue
+++ VUE-03/proyecto-rutas/src/views/Peliculas.vue
@@ -1,6 +1,12 @@
 <template>
 	<div>
+		<!-- <button @click="consumirPeliculas">Buscar películas</button> -->
 		<TituloPrincipal texto="PELÍCULAS" />
+		<div
+		v-for="p in peliculas"
+		:key="p.id">
+			{{p.title}}
+		</div>
 	</div>
 </template>
 
@@ -9,6 +15,39 @@ import TituloPrincipal from "../components/TituloPrincipal";
 export default {
 	components: {
 		TituloPrincipal,
+	},
+	data() {
+		return {
+			peliculas: [
+
+			]		
+		}
+	},
+	methods: {
+		//una forma de hacerlo
+		// consumirPeliculas(){
+		// 	fetch('https://ghibliapi.herokuapp.com/films')
+		// 	.then(res => res.json())
+		// 	.then(data => {
+		// 		console.log("fecth", data);
+		// 	})
+		// 	.catch(error => console.log(error));
+		// }
+
+		//segunda forma
+		async consumirPeliculas(){
+			try{
+				const res = await fetch('https://ghibliapi.herokuapp.com/films');
+				const data = await res.json();
+				console.log(data);
+				this.peliculas = data;
+			}catch(error){
+				console.log(error)
+			}
+		}
+	},
+	created() {
+		this.consumirPeliculas();
 	},
 };
 </script>
