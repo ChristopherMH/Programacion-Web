76fe9d21577285e4f84f8bd116431d56ebba7476
Christopher <christophermelendez13@gmail.com>
2021-05-03 16:29:08 -0600

    Inicio de proyecto con axios

---
+++ VUE-05/proyecto-va/src/store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    posts: [],
+    post: {}
+  },
+  mutations: {
+    SET_POSTS(state, posts){
+      state.posts = posts;
+    },
+    SET_POST(state, post){
+      state.post = post;
+    }
+  },
+  actions: {
+    getPosts({commit}){
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response =>{
+        commit('SET_POSTS', response.data)
+      })
+      .catch(error => console.log((error)))
+    }
+  },
+  modules: {
+  }
+})
---
+++ VUE-05/proyecto-va/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>Mis posts</h1>
+    <div v-for="p in posts"
+    :key="p.id">
+    <router-link :to="`/post/${p.id}`">
+      <h3>{{p.id}} - {{p.title}}</h3>
+    </router-link>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {mapState, mapActions} from 'vuex'
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  computed: {
+    ...mapState(['posts'])
+  },
+  methods: {
+    ...mapActions(['getPosts'])
+  },
+  created() {
+    this.getPosts()
+  },
+}
+</script>
